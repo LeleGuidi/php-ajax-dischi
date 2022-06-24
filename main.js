b341ab5ca6628dd11130e43e772c641d82596dc9
LeleGuidi <emanuele.guidi98@outlook.it>
2022-06-24 17:26:10 +0200

    Bonus completato

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const app = new Vue({
     el: '#app',
     data: {
+        albumsSelected: [],
         albums: [],
+        selectedGenre: '',
     },
     created() {
         axios.get('http://localhost:8888/php-ajax-dischi/server/api.php')
         .then((response) => {
+            this.albumsSelected = response.data
             this.albums = response.data
-        }) 
+        })
     },
     computed: {
         takeAllGenre() {
@@ -18,6 +21,19 @@ const app = new Vue({
                 }
             });
             return allGenre
+        }
+    },
+    methods: {
+        changeGenre() {
+            axios.get('http://localhost:8888/php-ajax-dischi/server/api.php', {
+                params: {
+                    genre: this.selectedGenre,
+                }
+            })
+            .then((response) => {
+                this.albumsSelected = response.data
+            })
+            console.log('ciao')
         }
     }
 })
--- server/api.php
+++ server/api.php
@@ -1,5 +1,15 @@
 <?php
     require __DIR__ . '/./database.php';
     header('Content-type: Application/json');
-    echo json_encode($database);
+    if (!empty($_GET['genre'])) {
+        foreach ($database as $album) {
+            if (in_array($_GET['genre'], $album)) {
+                $albums[] = $album;
+            }
+        }
+    } else {
+        $albums = $database;
+    }
+
+    echo json_encode($albums);
 ?>
